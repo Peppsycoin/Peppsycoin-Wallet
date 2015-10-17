33d9178dc8bdc4a3e971ea02bb6a2eafda816af0
ubu <ubu@mastermining.net>
2015-10-17 02:24:42 +0200

    Add DNS seed

--- src/net.cpp
+++ src/net.cpp
@@ -997,7 +997,7 @@ void ThreadSocketHandler2(void* parg)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-    {"5.149.51.217", "mastermining.net.org"},
+    {"5.149.51.217", "mastermining.net"},
 };
 
 void ThreadDNSAddressSeed(void* parg)
